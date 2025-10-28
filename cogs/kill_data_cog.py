6355c61d34a472568fa838799a73b8e4563eb5ea
jacta <jactanrob@outlook.com>
2025-10-28 16:26:07 +1300

    new kill stat saving bug fixes

--- cogs/kill_data_cog.py
+++ cogs/kill_data_cog.py
@@ -11,10 +11,10 @@ class kill_data_cog(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def kbacklog(self, ctx: Context, add_reaction: bool = False):
+    async def kbacklog(self, ctx: Context):
         total_added = 0
-        channel: discord.TextChannel = self.bot.get_channel(config.PURCHASE_STATS_CHANNEL)
-        this_last_seen = get_last_message(config.PURCHASE_STATS_CHANNEL)
+        channel: discord.TextChannel = self.bot.get_channel(config.KILLS_CHANNEL)
+        this_last_seen = get_last_message(config.KILLS_CHANNEL)
         await ctx.reply("okay! look in console for processing info")
         async for message in channel.history(limit=None):
             if message.id == this_last_seen:
@@ -22,14 +22,12 @@ class kill_data_cog(commands.Cog):
             if total_added == 0:
                 new_last_seen = message.id
             total_added += 1
-            bought_items, role, data_version = parse_purchase_message(message.content)
-            if data_version == "0.0":
-                break
-            with open(f'data/{data_version}.purchases.txt', 'a') as file:
-                file.write(f'{role}:{" ".join(bought_items)}\n')
+            data_version = message.content.split(" ")[0].removesuffix(")").removeprefix("(")
+            with open(f'data/{data_version}.kills.txt', 'a') as file:
+                file.write(f"{message.content}\n")
             print(f"{total_added} ADDED: {message.content}")
         await ctx.reply(f"Done! found {total_added} new data points")
-        set_last_message(config.PURCHASE_STATS_CHANNEL, new_last_seen)
+        set_last_message(config.KILLS_CHANNEL, new_last_seen)
 
 
 async def setup(bot):
